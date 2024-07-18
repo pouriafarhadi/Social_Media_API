56e13633f24d169e39172571df520b2c26db34d1
pouRia <pouria.f8410@gmail.com>
2024-07-18 10:12:40 +0330

    models configured

--- Dj_Drf_Social_Media_App/settings.py
+++ Dj_Drf_Social_Media_App/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps:
+    'users',
+    'notification',
+    'blog',
+    'chat',
+    'friend',
 ]
 
 MIDDLEWARE = [
